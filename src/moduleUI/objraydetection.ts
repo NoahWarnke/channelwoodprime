57931759b9e5cb0ab13abb0a1a9dc0e6029a4e94
surz <36134492+surz90@users.noreply.github.com>
2019-10-16 22:48:10 +0700

    Update objraydetection.ts

--- src/moduleUI/objraydetection.ts
+++ src/moduleUI/objraydetection.ts
@@ -6,57 +6,62 @@ const physicsCast = PhysicsCast.instance
 export class GateOrValveHit {
     hitGate = false
     hitValve = false
+    ray: Ray
+    timeCount = 0
     update(dt: number) {
+        this.timeCount += dt
+        if (this.timeCount > 0.1) {
+            this.timeCount = 0
+            this.ray = physicsCast.getRayFromCamera(4)
+            let depth = 3
+            let count = 0
 
-        let ray = physicsCast.getRayFromCamera(4)
-        let depth = 3
-        let count = 0
-
-        physicsCast.hitAll(ray, (e) => {
-            if (e.didHit) {
-                depth = 3
-                count = 0
-                this.hitGate = false
-                this.hitValve = false
-                for (let entityHit of e.entities) {
-                    count += 1
-                    //log(count)
-                    if (engine.entities[entityHit.entity.entityId].hasComponent(GLTFShape)) {
-                        if (engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/bridges/gate.glb') {
-                            //log(count, "HIT GATE")
-                            this.hitGate = true
+            physicsCast.hitAll(this.ray, (e) => {
+                if (e.didHit) {
+                    depth = 3
+                    count = 0
+                    this.hitGate = false
+                    this.hitValve = false
+                    for (let entityHit of e.entities) {
+                        count += 1
+                        //log(count)
+                        if (engine.entities[entityHit.entity.entityId].hasComponent(GLTFShape)) {
+                            if (engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/bridges/gate.glb') {
+                                //log(count, "HIT GATE")
+                                this.hitGate = true
+                                break
+                            }
+                            else if (engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/pipes/valveOnly.glb'
+                                || engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/pipes/pipeEdgeValveA.glb'
+                                || engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/pipes/pipeEdgeValveB.glb'
+                            ) {
+                                //log(count, "HIT VALVE")
+                                this.hitValve = true
+                                break
+                            }
+                            else {
+                            }
+                        }
+                        if (count === depth) {
+                            this.hitGate = false
+                            this.hitValve = false
                             break
                         }
-                        else if (engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/pipes/valveOnly.glb'
-                            || engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/pipes/pipeEdgeValveA.glb'
-                            || engine.entities[entityHit.entity.entityId].getComponent(GLTFShape).src === 'models/pipes/pipeEdgeValveB.glb'
-                        ) {
-                            //log(count, "HIT VALVE")
-                            this.hitValve = true
-                            break
-                        }
-                        else {
-                        }
-                    }
-                    if (count === depth) {
-                        this.hitGate = false
-                        this.hitValve = false
-                        break
                     }
                 }
-            }
-            else {
-                this.hitGate = false
-                this.hitValve = false
-            }
-            
-        })
+                else {
+                    this.hitGate = false
+                    this.hitValve = false
+                }
 
-        //log("GATE: ", this.hitGate, "VALVE", this.hitValve)
-        if (this.hitGate) UIclasses.UIObjects.hint.gateHint.show()
-        else UIclasses.UIObjects.hint.gateHint.hide()
+            })
 
-        if (this.hitValve) UIclasses.UIObjects.hint.valveHint.show()
-        else UIclasses.UIObjects.hint.valveHint.hide()
+            //log("GATE: ", this.hitGate, "VALVE", this.hitValve)
+            if (this.hitGate) UIclasses.UIObjects.hint.gateHint.show()
+            else UIclasses.UIObjects.hint.gateHint.hide()
+
+            if (this.hitValve) UIclasses.UIObjects.hint.valveHint.show()
+            else UIclasses.UIObjects.hint.valveHint.hide()
+        }
     }
 }
