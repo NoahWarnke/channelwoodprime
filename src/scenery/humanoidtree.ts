331e19e34a3331e83318d18cd7fa7886b5eb6331
Noah Warnke <noah.warnke@gmail.com>
2019-10-16 20:23:19 -0400

    Fix foliage on humaonid trees.

--- src/scenery/humanoidtree.ts
+++ src/scenery/humanoidtree.ts
@@ -9,21 +9,25 @@ export class HumanoidTree {
     new GLTFShape('models/trees/humanoidtree5_v2.glb'),
     new GLTFShape('models/trees/humanoidtree6_v2.glb'),
     new GLTFShape('models/trees/humanoidtree7_v2.glb'),
-  ]
+  ];
   
-  static leaves: GLTFShape = new GLTFShape('models/tree/foliage1.glb');
+  static heights: number[] = [
+    3.5, 3, 4.2, 4.2, 4, 3.4, 3.6
+  ];
+  
+  static leaves: GLTFShape = new GLTFShape('models/trees/foliage1.glb');
   
   constructor(transform: Transform, model: number) {
     
-    let trunk = new Entity;
+    let trunk = new Entity();
     trunk.addComponent(transform);
     trunk.addComponent(HumanoidTree.trunks[model]);
     
-    
-    let leaves = new Entity;
+    let leaves = new Entity();
     leaves.addComponent(HumanoidTree.leaves);
     leaves.addComponent(new Transform({
-      position: new Vector3(0, 10, 0)
+      position: new Vector3(0, HumanoidTree.heights[model], 0),
+      scale: new Vector3(0.15, 0.4, 0.15)
     }));
     leaves.setParent(trunk);
     
